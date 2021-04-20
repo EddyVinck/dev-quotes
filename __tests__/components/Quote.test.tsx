da5263513c8eddf25b0123c8c4acb709acd08427
Eddy Vinck <eddy@deity.io>
2021-04-20 20:09:15 +0200

    finish rest of the Quote tests

--- __tests__/components/Quote.test.tsx
+++ __tests__/components/Quote.test.tsx
@@ -1,7 +1,9 @@
-import { render, screen, waitFor } from "@testing-library/react";
+import { act, render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { getQuote } from "../../components/Quote/getQuote";
 import { Quote } from "../../components/Quote/Quote";
+import { QuoteResponse } from "../../pages/api/quote";
 
 jest.mock("../../components/Quote/getQuote", () => {
   const mockGetQuote = jest.fn().mockResolvedValue({
@@ -16,9 +18,28 @@ jest.mock("../../components/Quote/getQuote", () => {
   };
 });
 
+const mockQuotes: QuoteResponse[] = [
+  {
+    quote: "I'm going to make him an offer he can't refuse.",
+    author: "The Godfather",
+    id: 1,
+    error: "",
+  },
+  {
+    quote: "My precious.",
+    author: "The Lord of the Rings",
+    id: 2,
+    error: "",
+  },
+];
+
 describe("Quote", () => {
   beforeEach(() => {
     (getQuote as jest.Mock).mockClear();
+    jest.useFakeTimers();
+  });
+  afterEach(() => {
+    jest.useRealTimers();
   });
 
   test("renders the loader when there is no initial data", async () => {
@@ -52,9 +73,91 @@ describe("Quote", () => {
       /eddy vinck/i
     );
   });
-  test.todo('fetches a new quote when "new quote" is clicked');
+  test('fetches a new quote when "new quote" is clicked', async () => {
+    const queryClient = new QueryClient();
+    const MockQueryProvider: React.FC = ({ children }) => (
+      <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    );
+    render(
+      <MockQueryProvider>
+        <Quote />
+      </MockQueryProvider>
+    );
+    await waitFor(() => expect(getQuote).toHaveBeenCalledTimes(1));
+
+    ((getQuote as unknown) as jest.Mock<QuoteResponse>).mockImplementationOnce(
+      () => mockQuotes[0]
+    );
+    userEvent.click(screen.getByText(/new quote/i));
+    await waitFor(() => expect(getQuote).toHaveBeenCalledTimes(2));
+    const blockquote = screen.getByRole("figure");
+    expect(blockquote).toBeInTheDocument();
+    expect(blockquote).toHaveTextContent(/make him an offer/i);
+    expect(blockquote.querySelector("figcaption")).toHaveTextContent(
+      /the godfather/i
+    );
+  });
+
   describe("looping quotes", () => {
-    test.todo('continuously fetches a new quote when "loop quotes" is clicked');
-    test.todo('"new quote" is disabled when "loop quotes" is enabled');
+    test('continuously fetches a new quote when "loop quotes" is clicked', async () => {
+      const queryClient = new QueryClient();
+      const MockQueryProvider: React.FC = ({ children }) => (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      );
+      render(
+        <MockQueryProvider>
+          <Quote />
+        </MockQueryProvider>
+      );
+
+      // First, toggle the looping (1 request)
+      userEvent.click(await screen.findByText(/loop quotes/i));
+      await waitFor(() => expect(getQuote).toHaveBeenCalledTimes(1));
+      ((getQuote as unknown) as jest.Mock<QuoteResponse>).mockImplementationOnce(
+        () => mockQuotes[0]
+      );
+
+      // wait for getQuote to run (2 requests)
+      act(() => {
+        jest.runAllTimers();
+      });
+      await waitFor(() => expect(getQuote).toHaveBeenCalledTimes(2));
+
+      ((getQuote as unknown) as jest.Mock<QuoteResponse>).mockImplementationOnce(
+        () => mockQuotes[1]
+      );
+
+      // wait for getQuote to run (3 requests)
+      act(() => {
+        jest.runAllTimers();
+      });
+      await waitFor(() => expect(getQuote).toHaveBeenCalledTimes(3));
+
+      const blockquote = screen.getByRole("figure");
+      expect(blockquote).toBeInTheDocument();
+      expect(blockquote).toHaveTextContent(/precious/i);
+      expect(blockquote.querySelector("figcaption")).toHaveTextContent(
+        /lord of the rings/i
+      );
+    });
+
+    test('"new quote" is disabled when "loop quotes" is enabled', async () => {
+      const queryClient = new QueryClient();
+      const MockQueryProvider: React.FC = ({ children }) => (
+        <QueryClientProvider client={queryClient}>
+          {children}
+        </QueryClientProvider>
+      );
+      render(
+        <MockQueryProvider>
+          <Quote />
+        </MockQueryProvider>
+      );
+      await waitFor(() => expect(getQuote).toHaveBeenCalledTimes(1));
+      userEvent.click(screen.getByText(/loop quotes/i));
+      expect(screen.getByText(/new quote/i)).toBeDisabled();
+    });
   });
 });
